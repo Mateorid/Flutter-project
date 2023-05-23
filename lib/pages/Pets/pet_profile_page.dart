f02c0778fbf1e77d0f16d4560f38f4c0accade90
Matej Gorgol <56208414+Mateorid@users.noreply.github.com>
2023-05-23 21:11:47 +0200

    new pet profile page

--- lib/pages/Pets/pet_profile_page.dart
+++ lib/pages/Pets/pet_profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pet_sitting/Models/Pet/pet.dart';
@@ -6,7 +7,9 @@ import 'package:pet_sitting/services/date_service.dart';
 import 'package:pet_sitting/services/icon_service.dart';
 import 'package:pet_sitting/services/image_service.dart';
 import 'package:pet_sitting/styles.dart';
-import 'package:pet_sitting/widgets/core/info_tile.dart';
+import 'package:pet_sitting/widgets/ads/ad_detail_small_card.dart';
+import 'package:pet_sitting/widgets/core/basic_title.dart';
+import 'package:pet_sitting/widgets/core/outlined_container.dart';
 
 class PetProfilePage extends StatelessWidget {
   PetProfilePage({super.key, required this.pet});
@@ -18,17 +21,29 @@ class PetProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: 250,
-            actions: [_editButton(pet, context)],
-            flexibleSpace: FlexibleSpaceBar(background: _petPhoto()),
+    return SafeArea(
+      child: Scaffold(
+        body: CustomScrollView(
+          physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics(),
           ),
-          SliverList(delegate: _petInfo(context)),
-        ],
+          slivers: [
+            SliverAppBar(
+              floating: true,
+              expandedHeight: 250,
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: MAIN_GREEN,
+                ),
+                onPressed: () => {context.pop()},
+              ),
+              actions: [_editButton(pet, context)],
+              flexibleSpace: FlexibleSpaceBar(background: _petPhoto()),
+            ),
+            SliverList(delegate: _petInfo(context)),
+          ],
+        ),
       ),
     );
   }
@@ -45,56 +60,68 @@ class PetProfilePage extends StatelessWidget {
   }
 
   SliverChildListDelegate _petInfo(BuildContext context) {
-    final p = pet;
     return SliverChildListDelegate(
       [
-        InfoTile(
-          title: "Name",
-          content: _infoText(p.name),
-          icon: Icons.pets_rounded,
+        BasicTitle(text: pet.name),
+        const SizedBox(height: 15),
+        _buildInfoCards(pet),
+        const SizedBox(height: 15),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            'Description:',
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge
+                ?.copyWith(fontWeight: FontWeight.bold, color: DARK_GREEN),
+          ),
         ),
-        InfoTile(
-          title: "Species",
-          content: _infoText(p.species.text),
-          icon: Icons.info_outline,
+        OutlinedContainer(
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: Text(
+            _getDetailsText(pet.details),
+            style: const TextStyle(fontSize: 18),
+            textAlign: TextAlign.justify,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildInfoCards(Pet pet) {
+    return GridView.count(
+      crossAxisCount: 3,
+      childAspectRatio: 1.6,
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      children: [
+        AdDetailSmallCard(
+          icon: _iconService.getGenderIcon(pet.gender),
+          text: pet.gender.text,
         ),
-        InfoTile(
-          title: "Gender",
-          content: _infoText(p.gender.text),
-          icon: _iconService.getGenderIcon(p.gender),
+        AdDetailSmallCard(
+          icon: Icons.pets,
+          text: pet.species.text,
         ),
-        InfoTile(
-          title: "PetSize",
-          content: _infoText(p.size.text),
-          icon: _iconService.getSizeIcon(p.size),
+        AdDetailSmallCard(
+          icon: CupertinoIcons.arrow_up_left_arrow_down_right,
+          text: pet.size.text,
         ),
-        InfoTile(
-          title: "Breed",
-          content: _infoText(p.breed.toString()),
-          icon: Icons.pets_outlined,
-        ),
-        //todo make this tappable and onclick it will change from Bday to age (scale it)
-        InfoTile(
-          title: "Birthday",
-          content: _infoText(_getBirthdayText()),
-          icon: Icons.cake_outlined,
-          callback: () => print("//TODO: change to age"),
-        ),
-        //todo open images?
-        InfoTile(
-          title: "Photos",
-          content: _infoText("Click to show 9+ more photos"), //todo?
-          icon: Icons.image_outlined,
-          callback: () => print("//TODO: open photo page"),
-        ),
-        InfoTile(
-          title: "Details",
-          content: _infoText(p.details.toString().length > 30
-              ? "${p.details.toString().substring(0, 24)}..."
-              : p.details.toString()),
-          icon: Icons.question_mark,
-          callback: _detailsCallback(context, p.details.toString()),
-        ),
+        if (pet.breed != null && pet.breed != '')
+          AdDetailSmallCard(
+            icon: Icons.info_outline,
+            text: pet.breed!,
+          ),
+        if (pet.birthday != null)
+          AdDetailSmallCard(
+            icon: Icons.date_range,
+            text: _dateService.getPrintableDate(pet.birthday!),
+          ),
+        if (pet.birthday != null)
+          AdDetailSmallCard(
+            icon: Icons.cake,
+            text: _dateService.getAgeFromDate(pet.birthday!),
+          ),
       ],
     );
   }
@@ -109,29 +136,10 @@ class PetProfilePage extends StatelessWidget {
             style: TextStyle(fontSize: 20, color: MAIN_GREEN)));
   }
 
-  Widget _infoText(String text) {
-    return Text(text, style: const TextStyle(fontSize: 20));
-  }
-
-  String _getBirthdayText() {
-    final dt = pet.birthday;
-    return dt == null ? 'No info' : _dateService.getPrintableDate(dt);
-  }
-
-  VoidCallback _detailsCallback(BuildContext context, String text) {
-    return () {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          content: Text(text),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text("Close"),
-            )
-          ],
-        ),
-      );
-    };
+  String _getDetailsText(String? text) {
+    if (text == null || text == '') {
+      return 'Owner did not provide a description.';
+    }
+    return text;
   }
 }
